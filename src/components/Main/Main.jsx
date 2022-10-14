795f83b1a64b6a72cce89c3ed93c82340ae2d06d
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2022-10-14 11:47:57 +0530

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -37,16 +37,6 @@ const SERVERS = [
     username: 'openrelayproject',
     credential: 'openrelayproject',
   },
-  {
-    urls: 'turn:openrelay.metered.ca:443',
-    username: 'openrelayproject',
-    credential: 'openrelayproject',
-  },
-  {
-    urls: 'turn:openrelay.metered.ca:443?transport=tcp',
-    username: 'openrelayproject',
-    credential: 'openrelayproject',
-  },
 ];
 
 const AUDIO_THRESHOLD = 0.01;
